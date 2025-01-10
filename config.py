0ab56a7a312c8c2ce59deb5109c7eee91a2d0ed9
fgmnjkk <aws980891@gmail.com>
2025-01-10 07:57:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY EVIL4REAL
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7651280704:AAG8E5rEQkxYLAaNGTc6G6WvHKDAEeoM1XM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@LDX_COBRA")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7651280704")
+
+
+
+
+
+
+
+  
