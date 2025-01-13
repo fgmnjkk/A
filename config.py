eac23ea81aadb4094a1aaa4f16a53b115b6b3457
fgmnjkk <aws980891@gmail.com>
2025-01-13 18:44:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN=("7651280704:AAG8E5rEQkxYLAaNGTc6G6WvHKDAEeoM1XM")
 OWNER_USERNAME=("@LDX_COBRA")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("7651280704")
+ADMIN_IDS=("7509496491")
 
 
 
